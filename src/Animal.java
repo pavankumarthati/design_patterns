28c7ba003919ef57002bb3d9c5f40db14702207d
Pavan kumar <pavankumar.thati@gmail.com>
2017-01-30 17:54:33 +0530

    first commit

---
+++ src/Animal.java
@@ -0,0 +1,30 @@
+import sun.rmi.runtime.Log;
+
+/**
+ * Created by pavan on 30/1/17.
+ */
+public class Animal {
+    private String name;
+    private double height;
+    private double weight;
+    private String sound;
+
+    public void setName(String newName) { name = newName; }
+    public String getName() { return this.name; }
+
+    public void setHeight(double newHeight) { height = newHeight; }
+    public double getHeight() { return height; }
+
+    public void setWeight(double newWeight) {
+        if (newWeight > 0) {
+            weight = newWeight;
+        } else {
+            System.out.println("weight should be greater then 0");
+        }
+    }
+    public double getWeight() { return weight; }
+
+    public void setSound(String newSound) { sound = newSound; }
+    public String getSound() { return sound; }
+
+}
